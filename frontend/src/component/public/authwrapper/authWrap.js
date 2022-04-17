4cc4256f998cf9295284f12cf59ce3cd9fa5843c
ujjawal-12 <ujjawalkapoor2019@gmail.com>
2022-04-18 00:04:05 +0530

    create ui of home component

--- frontend/src/component/public/authwrapper/authWrap.js
+++ frontend/src/component/public/authwrapper/authWrap.js
@@ -4,26 +4,26 @@ import edu from "../../../images/img4.jpg";
 
 const AuthWrap = () => {
   return (
-     <div className='d-flex  align-items-center justify-content-center' style={{ "height": "575px" }}>
-        <div className="d-flex justify-content-center align-items-center" style={{ height: "530px", border: "1px solid grey", borderRadius: "25px",width:"50%" }}>
-          <div className=' h-100  d-flex justify-content-center align-items-center bg-success ' style={{ width:'45%', borderRadius: "3px" }}>
-            <img src={edu} alt="image" height="530px" width="307px" style={{"borderTopLeftRadius":'8px',"borderBottomLeftRadius":'8px',"opacity":'0.9'}}></img>
-          </div>
-          <div className=' h-100 ' style={{  borderLeft: ".1px grey", borderRadius: "3px" ,width:'55%'}}>
-            <div className="d-flex justify-content-center align-items-center flex-column " style={{padding:"15px"}} >
-              <div className='d-flex justify-content-around align-items-center w-100' style={{ borderRadius: "20px", paddingTop:"5px", backgroundColor: "black" }}>
-                <div className='h4' style={{ width: "50%", borderRight: "2px solid white", textAlign: "center", fontSize: "25px" }}><NavLink to="signin" style={({ isActive }) => isActive ? { textDecoration: "none", color: "green" } : { textDecoration: "none", color: "white" }}>SIGN-IN</NavLink ></div>
-                <div className='h4' style={{ width: "50%", textAlign: "center", fontSize: "25px" }}><NavLink to="signup" style={({ isActive }) => isActive ? { textDecoration: "none", color: "green" } : { textDecoration: "none", color: "white" }}>SIGN-UP</NavLink ></div>
-              </div>
+    <div className='d-flex  align-items-center justify-content-center' style={{ "height": "575px" }}>
+      <div className="d-flex justify-content-center align-items-center" style={{ height: "530px", border: "1px solid grey", borderRadius: "25px", width: "50%" }}>
+        <div className=' h-100  d-flex justify-content-center align-items-center' style={{ width: '45%', borderRadius: "15px" }}>
+          <img src={edu} alt="image" height="530px" width="307px" className='img-fluid w-100 h-100' style={{ "borderTopLeftRadius": '15px', "borderBottomLeftRadius": '15px', "opacity": '0.9' }}></img>
+        </div>
+        <div className=' h-100 ' style={{ borderLeft: ".1px grey", borderRadius: "3px", width: '55%' }}>
+          <div className="d-flex justify-content-center align-items-center flex-column " style={{ padding: "15px" }} >
+            <div className='d-flex justify-content-around align-items-center w-100' style={{ borderRadius: "20px", paddingTop: "5px", backgroundColor: "black" }}>
+              <div className='h4' style={{ width: "50%", borderRight: "2px solid white", textAlign: "center", fontSize: "25px" }}><NavLink to="signin" style={({ isActive }) => isActive ? { textDecoration: "none", color: "aqua" } : { textDecoration: "none", color: "white" }}>SIGN-IN</NavLink ></div>
+              <div className='h4' style={{ width: "50%", textAlign: "center", fontSize: "25px" }}><NavLink to="signup" style={({ isActive }) => isActive ? { textDecoration: "none", color: "aqua" } : { textDecoration: "none", color: "white" }}>SIGN-UP</NavLink ></div>
+            </div>
+            <div >
               <div >
-                <div >
                 <Outlet></Outlet>
-                </div>
               </div>
             </div>
           </div>
         </div>
-      </div >
+      </div>
+    </div >
   )
 }
 
--- frontend/src/component/public/root/root.js
+++ frontend/src/component/public/root/root.js
@@ -4,27 +4,27 @@ import { Outlet, NavLink } from 'react-router-dom';
 import Avatar from 'react-avatar';
 import img from "../../../images/logo.png";
 import style from "./root.module.css";
-import {ImHome} from "react-icons/im";
+import { ImHome } from "react-icons/im";
 import { HiAcademicCap } from "react-icons/hi";
 import { AiFillContacts } from "react-icons/ai";
 import 'antd/dist/antd.css';
-import {RiUserSettingsFill,RiBankFill,RiChatPollFill} from "react-icons/ri";
+import { RiUserSettingsFill, RiBankFill, RiChatPollFill } from "react-icons/ri";
 import { GrUserNew } from "react-icons/gr";
 import { IoIosArrowDown } from "react-icons/io";
-import { FaUserAlt ,FaHandPointRight } from "react-icons/fa";
-import {  Menu, Dropdown  } from "antd"
+import { FaUserAlt, FaHandPointRight } from "react-icons/fa";
+import { Menu, Dropdown } from "antd"
 import { DownOutlined } from '@ant-design/icons';
 
-const linkActive = { "borderTop": "2px solid aqua", "color": "aqua", "textDecoration": "none", "paddingBottom": "9px","paddingTop": "9px" }
+const linkActive = { "borderTop": "2px solid aqua", "color": "aqua", "textDecoration": "none", "paddingBottom": "9px", "paddingTop": "9px" }
 const defaultLink = { "textDecoration": "none", "color": "white" }
 
 const menu = (
   <Menu>
     <Menu.Item key="0">
-     <NavLink to="academic">Profile</NavLink>
+      <NavLink to="academic">Profile</NavLink>
     </Menu.Item>
     <Menu.Item key="1">
-     <NavLink to="/">Logout</NavLink>
+      <NavLink to="/">Logout</NavLink>
     </Menu.Item>
   </Menu>
 );
@@ -34,25 +34,25 @@ export const Root = () => {
   return (
     <div>
       <div className="container-fluid bg-dark mb-3" style={{ display: "flex" }} >
-        <div  style={{ width: "15%", margin: "10px 1px 10px 2PX " }}><img className="rounded-3 img-fluid ms-3" src={img} width="180px" height="100px"></img></div>
-        <div className='container ' style={key?{ width: "70%",paddingLeft:"45px" }:{ width: "80%",paddingLeft:"55px",paddingRight:"60px" }}>
+        <div style={{ width: "15%", margin: "10px 1px 10px 2PX " }}><img className="rounded-3 img-fluid ms-3" src={img} width="180px" height="100px"></img></div>
+        <div className='container ' style={key ? { width: "70%", paddingLeft: "45px" } : { width: "80%", paddingLeft: "55px", paddingRight: "60px" }}>
           <marquee width="100%" direction="left" scrollamount="13">
             <div style={{ marginTop: "25px", color: "aqua", fontSize: "25px" }}>WELCOME TO NAAC A+ AFFILIATED COLLEGE IN UTTARAKHAND</div>
           </marquee>
         </div>
         {key && <div className='container d-flex justify-content-center align-items-center' style={{ width: "15%" }}>
-        <Avatar name={sessionStorage.getItem("loginusername")} size="50" round={true} />
-        <Dropdown overlay={menu} trigger={['click']}>
-    <span className="ant-dropdown-link text-light ms-2 " style={{"fontSize":"22px"}} onClick={e => e.preventDefault()}>
-      <IoIosArrowDown />
-    </span>
-  </Dropdown></div>}
+          <Avatar name={sessionStorage.getItem("loginusername")} size="50" round={true} />
+          <Dropdown overlay={menu} trigger={['click']}>
+            <span className="ant-dropdown-link text-light ms-2 " style={{ "fontSize": "22px" }} onClick={e => e.preventDefault()}>
+              <IoIosArrowDown />
+            </span>
+          </Dropdown></div>}
       </div>
-      <div className='container bg-dark d-flex mt-2 rounded fs-5 py-4' style={{ "height": "50px","color":"teal" }} >
+      <div className='container-md bg-dark d-flex mt-2 rounded fs-5 py-4' style={{ "height": "50px", "color": "teal" }} >
         <div
           className=' d-flex justify-content-center align-items-center mx-1 h-100'
           style={{ "width": "100px" }}>
-          <NavLink to={key? "home" : "/"} style={({ isActive }) => isActive ? linkActive :
+          <NavLink to={key ? "home" : "/"} style={({ isActive }) => isActive ? linkActive :
             defaultLink}><span><ImHome></ImHome> </span>Home</NavLink>
         </div>
         <div
@@ -73,7 +73,7 @@ export const Root = () => {
           <NavLink to="contact" style={({ isActive }) => isActive ? linkActive :
             defaultLink}><span ><AiFillContacts></AiFillContacts> </span>contact</NavLink>
         </div>
-        
+
         {/* <div 
         className=' d-flex justify-content-center align-items-center mx-1' 
         style={{ "width": "100px" }}>
@@ -86,28 +86,28 @@ export const Root = () => {
           <NavLink to="addmission" style={({ isActive }) => isActive ? linkActive :
             defaultLink} ><span><RiBankFill></RiBankFill> </span>Addmission</NavLink>
         </div>
-        { key && <><div
+        {key && <><div
           className=' d-flex justify-content-center align-items-center mx-1'
           style={{ "width": "120px" }}>
           <NavLink to="program" style={({ isActive }) => isActive ? linkActive :
             defaultLink}><span ><RiUserSettingsFill></RiUserSettingsFill> </span>Program</NavLink>
         </div>
-        <div
-          className=' d-flex justify-content-center align-items-center mx-1'
-          style={{ "width": "100px" }}>
-          <NavLink to="apply" style={({ isActive }) => isActive ? linkActive :
-            defaultLink}><span><FaHandPointRight></FaHandPointRight> </span>Apply</NavLink>
-        </div></>}
+          <div
+            className=' d-flex justify-content-center align-items-center mx-1'
+            style={{ "width": "100px" }}>
+            <NavLink to="apply" style={({ isActive }) => isActive ? linkActive :
+              defaultLink}><span><FaHandPointRight></FaHandPointRight> </span>Apply</NavLink>
+          </div></>}
         <div className=' d-flex justify-content-center align-items-center ms-auto'
-            style={{ "width": key?"160":"180" }}>
-        {key ?<NavLink to="logout" style={({ isActive }) => 
-        isActive ? linkActive : defaultLink}><span ><FaUserAlt></FaUserAlt> </span> Logout</NavLink> :
-        (<><span ><FaUserAlt></FaUserAlt> </span>&#160;<NavLink to="auth/signin" style={({ isActive }) =>
-         isActive ? linkActive : defaultLink}>SignIn</NavLink> &#160;<span style={{"color":"white"}}>/</span>
-         <NavLink to="auth/signup" style={({ isActive }) =>
-         isActive ? linkActive : defaultLink}>&#160;SignUp</NavLink></>)
-         }
-      </div>
+          style={{ "width": key ? "160" : "180" }}>
+          {key ? <NavLink to="logout" style={({ isActive }) =>
+            isActive ? linkActive : defaultLink}><span ><FaUserAlt></FaUserAlt> </span> Logout</NavLink> :
+            (<><span ><FaUserAlt></FaUserAlt> </span>&#160;<NavLink to="auth/signin" style={({ isActive }) =>
+              isActive ? linkActive : defaultLink}>SignIn</NavLink> &#160;<span style={{ "color": "white" }}>/</span>
+              <NavLink to="auth/signup" style={({ isActive }) =>
+                isActive ? linkActive : defaultLink}>&#160;SignUp</NavLink></>)
+          }
+        </div>
       </div>
       <Outlet></Outlet>
     </div >
